486170e6a0da800e689a16483d20db1ca7aa15ce
Bill Adams <vikingbill@gmail.com>
2021-03-05 17:06:10 -0500

    add winner functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,50 @@
+let gameStatus = "";
+
 const imgX = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg";
 const imgO = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg";
 
 let currentPlayerSymbol = "x";
 let squareValues = ["", "", "", "", "", "", "", "", "",];
+function checkGameStatus() {
+    if (squareValues[0] === squareValues[1] && squareValues[1] === squareValues[2] && squareValues[2] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    else if (squareValues[3] === squareValues[4] && squareValues[4] === squareValues[5] && squareValues[5] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    else if (squareValues[6] === squareValues[7] && squareValues[7] === squareValues[8] && squareValues[8] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    else if (squareValues[0] === squareValues[3] && squareValues[3] === squareValues[6] && squareValues[6] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    else if (squareValues[1] === squareValues[4] && squareValues[4] === squareValues[7] && squareValues[7] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    else if (squareValues[2] === squareValues[5] && squareValues[5] === squareValues[8] && squareValues[8] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    else if (squareValues[2] === squareValues[4] && squareValues[4] === squareValues[6] && squareValues[6] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    else if (squareValues[0] === squareValues[4] && squareValues[4] === squareValues[8] && squareValues[8] !== ''){
+        gameStatus = currentPlayerSymbol;
+    }
+    // else {
+    //     gameStatus = 'None'
+    // }
+    if (gameStatus !== ''){
+        document.getElementById('game-status').innerHTML = 'Winner : ' + gameStatus.toUpperCase();
+    }
+}
 
 window.addEventListener("DOMContentLoaded", (event) => {
     const board = document.getElementById("tic-tac-toe-board");
     console.log(board);
     board.addEventListener("click", (event) => {
+        if (gameStatus !== '') {
+            return;
+        }
         const square = event.target;
         console.log(square);
         console.log(square.id);
@@ -25,6 +62,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
             img.setAttribute("src", imgX);
             square.appendChild(img);
             squareValues[squareNum] = currentPlayerSymbol;
+            checkGameStatus();
             currentPlayerSymbol = "o";
         }
         else {
@@ -32,7 +70,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
             img.setAttribute("src", imgO);
             square.appendChild(img);
             squareValues[squareNum] = currentPlayerSymbol;
+            checkGameStatus();
             currentPlayerSymbol = "x";
         }
+
     });
 });
